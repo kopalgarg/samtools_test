d1226a44d9f49e5187986af22e06b9e8609357f9
kopalgarg <kopalgarg24@gmail.com>
2023-09-28 11:32:27 -0700

    Delete unit_tests.py

--- unit_tests.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def test_docker_build():
-    try:
-        subprocess.run(["docker", "build", "-t", "samtools", "."], check=True)
-    except subprocess.CalledProcessError:
-        assert False, "Docker build failed"
-
-test_docker_build()
